63e8926d5aaa98170628bb20f4e0fe4777bbd4f0
Piotr Połomski <pio7rek@gmail.com>
2013-10-31 19:15:23 +0100

    test 1

---
+++ ksiazkacpp5/src/ksiazkacpp5.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ksiazkacpp5.cpp
+// Author      : ja
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
